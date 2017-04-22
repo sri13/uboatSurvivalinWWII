30c07e43dabbf4bf9cf02e9465932b99b7eb7341
sri13 <srikanth.tulasiram@gmail.com>
2017-04-22 22:13:45 +0100

    General Scenario done

--- 16201018_16200947_16203106.py
+++ 16201018_16200947_16203106.py
@@ -1,15 +1,57 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-
+from geopy.distance import vincenty
 
 #minimum number of nodes required for communication
 # 1- HQ; 1- CC and 1 - Uboat
 minNumNodes = 3
 
-# from geopy.distance import vincenty
-# newport_ri = (41.49008, -71.312796)
-# cleveland_oh = (41.499498, -81.695391)
-# print(vincenty(newport_ri, cleveland_oh).kilometers)
+minTransRange = 300
+minViewRange = 50
+
+
+def set_edges(graph):
+    
+    for eachNode in graph.nodes():
+        
+        for nextNode in graph.nodes():
+            
+            # not same node in two loops and no conncetion
+            if (eachNode != nextNode) and \
+             not graph.has_edge(eachNode, nextNode):
+                 
+                 # for HQ node
+                 if (graph.node[eachNode]["type"] == "HQ"):
+                    if (graph.node[nextNode]["type"] == "CC"):
+                        graph.add_edge(eachNode,nextNode)
+                        graph.edge[eachNode][nextNode]["weight"] = 1
+                
+                # for Command Center node
+                 elif(graph.node[eachNode]["type"] == "CC"):
+                     if (graph.node[nextNode]["type"] == "HQ") or \
+                            (graph.node[nextNode]["type"] == "CC") or \
+                                  ( (graph.node[nextNode]["type"] == "Uboat") \
+                                     and (graph.node[nextNode]["listeningZone"] == 0) ):
+                        graph.add_edge(eachNode,nextNode)
+                        graph.edge[eachNode][nextNode]["weight"] = 1
+
+                # for Uboat node
+                 elif(graph.node[eachNode]["type"] == "Uboat"):
+                     
+                     eachNodeLoc= (graph.node[eachNode]["lat"],graph.node[eachNode]["lon"])
+                     nextNodeLoc= (graph.node[nextNode]["lat"],graph.node[nextNode]["lon"])
+                        
+                     if(graph.node[nextNode]["type"] == "Uboat") and \
+                        ( vincenty(eachNodeLoc, nextNodeLoc).kilometers <= minTransRange ):
+                        graph.add_edge(eachNode,nextNode)
+                        graph.edge[eachNode][nextNode]["weight"] = 1
+
+                     elif(graph.node[nextNode]["type"] == "convoy") and \
+                          (vincenty(eachNodeLoc, nextNodeLoc).kilometers <= minViewRange ):
+                            graph.add_edge(eachNode,nextNode)
+                            graph.edge[eachNode][nextNode]["weight"] = 1
+    return graph
+
 
 def set_HQ_CC(graph, HQNode, CCNode):
 
@@ -24,18 +66,18 @@ def set_HQ_CC(graph, HQNode, CCNode):
         raise ValueError("CC node " + str(CCNode) + " not present")
 
     #HQ setup
-    graph.node[HQNode]["lat"] = 52.518912
-    graph.node[HQNode]["lon"] = 13.398652
+    graph.node[HQNode]["lat"] = 52.51
+    graph.node[HQNode]["lon"] = 13.39
     graph.node[HQNode]["type"] = "HQ"
 
     #CC Setup
-    graph.node[CCNode]["lat"]=47.748208
-    graph.node[CCNode]["lon"]=-3.369157
+    graph.node[CCNode]["lat"]=50.74
+    graph.node[CCNode]["lon"]=-3.36
     graph.node[CCNode]["type"] = "CC"
 
     #add edge between HQ and CC
     graph.add_edge(HQNode,CCNode)
-    graph[HQNode][CCNode] = 1
+    graph[HQNode][CCNode]["weight"] = 1
     
     return graph
 
@@ -46,8 +88,8 @@ def set_convoy(graph, ConvoyNode):
         raise ValueError("Convoy Node " + str(ConvoyNode) + " not present")
 
     #Convoy setup
-    graph.node[ConvoyNode]["lat"] = 52.486730
-    graph.node[ConvoyNode]["lon"] = -45.983332
+    graph.node[ConvoyNode]["lat"] = 52.48
+    graph.node[ConvoyNode]["lon"] = -59.98
     graph.node[ConvoyNode]["type"] = "convoy"
 
     return graph
@@ -102,27 +144,40 @@ def get_graph(numOfNodes=10):
     graph = set_HQ_CC(graph,0,1)
 
     #set Nodes as Uboats
-    UboatLocations = ([52.526676, -45.983332],
-                      [51.026676, -44.983332],
-                      [54.026676, -44.983332],
-                      [52.7, -43.98564],
-                      [54.0, -42.0],
-                      [52.7, -40.98564],
-                      [50.0, -42.0])
+    UboatLocations = ([52.48, -59.28], # Node -2
+                      [52.00, -55.78], # Node -3
+                      [54.50, -57.78], # Node -4
+                      [53.68, -54.28], # Node -5
+                      [53.48, -50.58], # Node -6
+                      [53.00, -46.98], # Node -7
+                      [52.98, -43.26], # Node -8
+                      [51.50, -39.65], # Node -9
+                      [52.45, -35.98], # Node -10
+                      [52.43, -32.31], # Node -11
+                      [51.10, -28.67], # Node -12
+                      [54.18, -28.96], # Node -13
+                      [52.91, -35.21], # Node -14
+                      [52.50, -26.00]) # Node -15
 
-    numOfUboats = 7
+    numOfUboats = 14
 
     graph = set_Uboats(graph, numOfUboats, UboatLocations)
 
     #set convoy node
-    graph = set_convoy(graph,9)
+    graph = set_convoy(graph,16)
+    
+    #set edges
+    graph = set_edges(graph)
     
     return graph
 
 def draw_graph(graph):
+    
+    pos = {}
 
-    pos = nx.layout.random_layout(graph)
-
+    for eachNode in graph.nodes():
+        pos[eachNode]=(graph.node[eachNode]["lon"],graph.node[eachNode]["lat"])
+        
     # get three subsets of nodes: susceptible, infected, removed
     HQ_node = [n for n in graph.nodes() if graph.node[n]["type"] == "HQ"]
     convoy_node = [n for n in graph.nodes() if graph.node[n]["type"] == "convoy"]
@@ -135,24 +190,38 @@ def draw_graph(graph):
     
 
     # draw edges, then draw each subset in different colour
-    nx.draw_networkx_edges(graph, pos)
+    
     nx.draw_networkx_nodes(graph, pos, nodelist=HQ_node, node_color="m")
     nx.draw_networkx_nodes(graph, pos, nodelist=convoy_node, node_color="r")
     nx.draw_networkx_nodes(graph, pos, nodelist=CC_nodes, node_color="c")
     nx.draw_networkx_nodes(graph, pos, nodelist=Uboat_lz_nodes, node_color="b")
     nx.draw_networkx_nodes(graph, pos, nodelist=Uboat_nlz_nodes, node_color="g")
     nx.draw_networkx_nodes(graph, pos, nodelist=empty_nodes, node_color="w")
-    plt.savefig("UboatSurvival.png") # pad filename with zeros
-    plt.close()
 
+    nx.draw_networkx_labels(graph, pos)
+    
+    nx.draw_networkx_edges(graph, pos)
+#    nx.draw_networkx_edge_labels(graph, pos)
+
+    plt.savefig("UboatSurvival.png") 
+    plt.close()
 
         
 if __name__ == "__main__":
-    graph  = get_graph(10)
-
+    
+    plt.figure(figsize=(15,15))
+    
+    graph  = get_graph(17)
+    
     print(graph.nodes(data=True))
 
-    print(graph.edges())
+    print(graph.edges(data=True))
 
     draw_graph(graph)
+    
+    print("Shortest Path - " ,nx.dijkstra_path(graph,16,0))
+    
+    print("Shortest path Length - ",nx.dijkstra_path_length(graph,16,0))
+        
+    print("Single Source Shortest Path - " ,nx.dijkstra_path(graph,16,0))
     
